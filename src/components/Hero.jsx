0683a1ae92ae5a6f908d7e5623407d4305f2d00a
xrmaalk <xinra.inc@gmail.com>
2024-06-09 20:12:17 -0600

    setup Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from "react"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { ComputersCanvas } from "./canvas"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 h-40 sm:h-80  violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            How &apos;dy, &nbsp; I&apos;m &nbsp;
+            <span className="text-[#915eff]">Maalkum</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develope 3D visuals, native applications, user interfaces, and web
+            applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
   )
 }
 
