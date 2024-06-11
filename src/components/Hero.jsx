b9f35916a3269f3962fc2e682f669763f55c78a1
xrmaalk <xinra.inc@gmail.com>
2024-06-11 07:33:07 -0600

    adjust import statment maath/misc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { ComputersCanvas } from "./canvas"
+import { repeat } from "maath/misc"
 
 const Hero = () => {
   return (
@@ -19,11 +20,29 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am an intermediate Software Developer. <br />I develope 3D models,
-            visuals, user interfaces, web and native applications.
+            visuals, unique user experiences, web and native applications.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify center items-start p-2+">
+            <motion.dev
+              animate={{
+                y: [0, 33, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
