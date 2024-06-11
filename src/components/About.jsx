1d7d40f000ff19e84a92853a0ddc070348c402fc
xrmaalk <xinra.inc@gmail.com>
2024-06-11 17:30:55 -0600

    refactor Story text => constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactCardFlip from "react-card-flip"
 import { Tilt } from "react-tilt"
 import { motion } from "framer-motion"
 import { styles } from "../styles"
-import { services } from "../constants"
+import { textBlocks, services } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
 import { ServiceCard } from "./ServiceCard"
 import { SectionWrapper } from "../hoc/SectionWrapper"
 
 const About = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % textBlocks.length)
+  }
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -19,67 +25,17 @@ const About = () => {
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-amber-400 text-[17px] max-w-3xl leading-[30px] flex-1 items-center justify-center ">
-        This section walks you through the rich history behind Maalkum the
-        Software Developer...
-        <br />
-        He started programming in the early 90&apos;s with Java.
+        className="mt-4 text-amber-400 text-[25px] max-w-3xl leading-[30px] flex-1 items-center justify-center ">
+        {textBlocks[currentIndex]}
         <br />
         <br />
-        In 2010, he begun learning PHP, later moving on to PYTHON.
-        <br />
-        Back then, He sharped his skills by providing customize business
-        websites via wordpress.
-        <br />
-        <br />
-        After landing a role as a Teritory Manager, he took an hiatus from
-        programming, focusing on a career as a Sales Professional.
-        <br />
-        <br /> Maalkum would always say: <br />
-        <br />
-        <b>
-          <i>
-            &quot;My passion is in making unique experiences for users and
-            clients alike.&quot;
-          </i>
-        </b>{" "}
-        &nbsp;
-        <br />
-        <br />
-        It is within this spirit that he stumbbled came across a social media
-        challenge....
-        <br />
-        <b>
-          <i>&quot;the 100 Day of Code Challenege....&quot;</i>
-        </b>{" "}
-        , late August 2020.
-        <br /> <br /> While working fulltime as a Senior Solutions Sales
-        Executive and handling the streamline of a promotional item he
-        developed, Maalkum was able to complete the 100 days of code Challenge.
-        <br />
-        <br /> He completed the challenege using Python which at the time he
-        recalls:
-        <br />{" "}
-        <i>&quot;the move away from the Java syntax was a nice change.&quot;</i>
-        <br />
-        <br />
-        Fast-forward to May 2023, Maalkum found himself back in the job market.
-        His employer at that time made the business decision to departure from
-        the Canadian Auction Market.
-        <br />
-        <br />
-        When one door closes another surely opens.
-        <br />
-        Maalkum was presented with the opprotunity to get formal training into
-        Sotware Development through InceptionU. He pounced jumped at the
-        opprotunity. <br />
-        <br />
-        Which leads us to why you&apos;ve stopped by....
+        <button onClick={handleNextClick}>Click to continue Story....</button>
+
         <br />
         <br />
         <h1>
           <b>
-            Explore Projects&apos; or submit a the contact form to get in touch.
+            Explore Projects&apos; or submit a contact form to get in touch.
           </b>
         </h1>
         <br />
