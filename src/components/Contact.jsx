0e57bcaf014a833cfc03098c44a5ddc78b46608d
xrmaalk <xinra.inc@gmail.com>
2024-06-13 22:50:03 -0600

    add emailjs package

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,23 @@
 import { useState, useRef } from "react"
 import { EarthCanvas } from "./canvas"
 import { motion } from "framer-motion"
-import { emailjs } from "@emailjs/browser"
+import emailjs from "@emailjs/browser"
 import { styles } from "../styles"
 import { slideIn } from "../utils/motion"
 
 import { SectionWrapper } from "../hoc/SectionWrapper"
 
 const Contact = () => {
+  const formRef = useRef()
+  const [loader, setLoader] = useState(false)
   const [form, setForm] = useState({
     name: "",
     email: "",
     message: "",
   })
+
+  const handleChange = (e) => {}
+  const handleSubmit = (e) => {}
 
   return <div>Contact</div>
 }
